ca29f4a0fdd4311ab958db92174001671a34c7f6
Arti_Dev <boomcreeper296@gmail.com>
2022-06-09 22:28:52 -0400

    Addition of crafting recipes, durability, and persistent data containers

--- src/main/java/com/articreep/frostboats/FrostBoats.java
+++ src/main/java/com/articreep/frostboats/FrostBoats.java
@@ -2,25 +2,78 @@ package com.articreep.frostboats;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import javax.naming.Name;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public final class FrostBoats extends JavaPlugin {
     private static FrostBoats plugin;
+    private static final List<NamespacedKey> recipeKeys = new ArrayList<>();
+    public static final int maxDurability = 1000;
 
     @Override
     public void onEnable() {
         plugin = this;
         getServer().getPluginManager().registerEvents(new Listeners(), this);
-        Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "Get frosting! Plugin loaded.");
 
+        // Recipe time!
+        Material[] materials = {Material.ACACIA_BOAT, Material.BIRCH_BOAT, Material.DARK_OAK_BOAT, Material.JUNGLE_BOAT,
+                Material.MANGROVE_BOAT, Material.OAK_BOAT, Material.SPRUCE_BOAT, Material.ACACIA_CHEST_BOAT, Material.BIRCH_CHEST_BOAT,
+                Material.DARK_OAK_CHEST_BOAT, Material.JUNGLE_CHEST_BOAT, Material.MANGROVE_CHEST_BOAT, Material.OAK_CHEST_BOAT, Material.SPRUCE_CHEST_BOAT};
+
+        for (Material material : materials) {
+            // Create the product of the recipe
+            ItemStack product = new ItemStack(material);
+            product.addUnsafeEnchantment(Enchantment.FROST_WALKER, 2);
+            ItemMeta meta = product.getItemMeta();
+            meta.setLore(Collections.singletonList(ChatColor.DARK_GRAY + "Durability: " + maxDurability));
+            product.setItemMeta(meta);
+
+            // Create the key and make the recipe
+            NamespacedKey key = new NamespacedKey(this, "frosted_" + material.toString().toLowerCase());
+            recipeKeys.add(key);
+            ShapedRecipe recipe = new ShapedRecipe(key, product);
+            recipe.shape("   ", "PBP", "SSS");
+            recipe.setIngredient('P', Material.POWDER_SNOW_BUCKET);
+            recipe.setIngredient('B', material);
+            recipe.setIngredient('S', Material.SNOW_BLOCK);
+
+            // Register the recipe
+            Bukkit.addRecipe(recipe);
+
+            // Give everyone online the recipe when loaded if they don't already have it
+            for (Player p : Bukkit.getOnlinePlayers()) {
+                p.discoverRecipe(key);
+            }
+        }
+
+        Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "Get frosting! Plugin loaded.");
     }
 
     @Override
     public void onDisable() {
-        Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "Goodbye!");
+        for (NamespacedKey key : recipeKeys) {
+            Bukkit.removeRecipe(key);
+        }
+
+        Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "Unloaded recipes!");
     }
 
     public static FrostBoats getPlugin() {
         return plugin;
+    }
+
+    public static List<NamespacedKey> getRecipeKeys() {
+        return recipeKeys;
     }
 }
--- src/main/java/com/articreep/frostboats/Listeners.java
+++ src/main/java/com/articreep/frostboats/Listeners.java
@@ -3,38 +3,56 @@ package com.articreep.frostboats;
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Boat;
-import org.bukkit.entity.ChestBoat;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPlaceEvent;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.vehicle.VehicleDestroyEvent;
 import org.bukkit.event.vehicle.VehicleMoveEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 
-import java.util.HashMap;
+import java.util.Collections;
 
 public class Listeners implements Listener {
-
-    public final static HashMap<Integer, Integer> frostBoats = new HashMap<>();
+    NamespacedKey frostWalkerKey = new NamespacedKey(FrostBoats.getPlugin(), "frost-walker-level");
+    NamespacedKey durabilityKey = new NamespacedKey(FrostBoats.getPlugin(), "durability");
 
     @EventHandler
     public void onBoatPlace(EntityPlaceEvent event) {
         if (event.getEntityType() == EntityType.BOAT || event.getEntityType() == EntityType.CHEST_BOAT) {
+
             ItemStack item = event.getPlayer().getInventory().getItemInMainHand();
+            Entity boat = event.getEntity();
 
             if (item.getEnchantments().isEmpty()) return;
+
+            PersistentDataContainer itemContainer = item.getItemMeta().getPersistentDataContainer();
+            PersistentDataContainer entityContainer = boat.getPersistentDataContainer();
 
             // Check enchantments for frost walker
             for (Enchantment enchantment : item.getEnchantments().keySet()) {
                 if (enchantment.equals(Enchantment.FROST_WALKER)) {
 
                     // Cool, it's a frost walker boat.
-                    Boat boat = (Boat) event.getEntity();
-                    // Map boat entity ID to Frost Walker Level
-                    frostBoats.put(boat.getEntityId(), item.getEnchantments().get(enchantment));
+
+                    // Add Frost Walker level to the Boat's PersistentDataContainer
+                    entityContainer.set(frostWalkerKey, PersistentDataType.INTEGER, item.getEnchantments().get(enchantment));
+
+                    // Does this boat have a durability value yet?
+                    if (!itemContainer.has(durabilityKey, PersistentDataType.INTEGER)) {
+                        entityContainer.set(durabilityKey, PersistentDataType.INTEGER, FrostBoats.maxDurability);
+                    } else {
+                        // grab the value
+                        entityContainer.set(durabilityKey, PersistentDataType.INTEGER,
+                                itemContainer.get(durabilityKey, PersistentDataType.INTEGER));
+                    }
+
 
                     // Since boats sink a little, spawn the boat just a little higher if spawned in water
                     if (event.getBlock().getType() == Material.WATER) {
@@ -50,44 +68,76 @@ public class Listeners implements Listener {
     @EventHandler
     public void onBoatMove(VehicleMoveEvent event) {
 
-        // Is it a frostboat?
-        if (frostBoats.containsKey(event.getVehicle().getEntityId())) {
+        Entity boat = event.getVehicle();
+        PersistentDataContainer container = boat.getPersistentDataContainer();
+        boolean hasGeneratedIce = false;
 
-            Boat boat = (Boat) event.getVehicle();
+        if (!container.has(frostWalkerKey, PersistentDataType.INTEGER)) return;
+
+        // Is it a frostboat?
+        if (container.get(frostWalkerKey, PersistentDataType.INTEGER) > 0) {
+
+            // Spawn a cute particle
+            boat.getLocation().getWorld().spawnParticle(Particle.SNOWFLAKE, boat.getLocation(), 1, 0, 0, 0, 0);
 
             // Get the blocks underneath the boat - nested for loops
-            int radius = 2 + frostBoats.get(boat.getEntityId());
+            int radius = 5 + container.get(frostWalkerKey, PersistentDataType.INTEGER);
             // Cap the radius
             if (radius > 15) radius = 15;
+            int radiusSquared = radius * radius;
 
+            Location center = boat.getLocation().clone().getBlock().getLocation().subtract(0, 1, 0);
 
-            Location center = boat.getLocation().clone().subtract(0, 1, 0);
-
-            // Should iterate over 25 blocks
             for (int x = center.getBlockX() - radius; x <= center.getBlockX() + radius; x++) {
                 for (int z = center.getBlockZ() - radius; z <= center.getBlockZ() + radius; z++) {
+                    Location l = new Location(center.getWorld(), x, center.getY(), z);
+                    if (l.distanceSquared(center) > radiusSquared) continue;
+                    Block block = l.getBlock();
 
-                    Block block = new Location(center.getWorld(), x, center.getY(), z).getBlock();
-
-                    // If it's a water block change it to ice.
+                    // If it's a water block AND there is air above change it to ice.
                     if (block.getType() == Material.WATER) {
-                        block.setType(Material.FROSTED_ICE);
+                        if (l.add(0, 1, 0).getBlock().getType() == Material.AIR) {
+                            block.setType(Material.FROSTED_ICE);
+                            // Indicate that the boat has frosted ice
+                            if (!hasGeneratedIce) hasGeneratedIce = true;
+                        }
                     }
                 }
             }
+
+            // After all this is done, decrement the durability of the boat down by 1.
+            // unless it didn't frost anything, in that case just return as there's nothing else to do
+            if (!hasGeneratedIce) return;
+
+            int durability = container.get(durabilityKey, PersistentDataType.INTEGER);
+            // Durability ran out?
+            if (durability - 1 <= 0) {
+                for (Entity entity : boat.getPassengers()) {
+                    entity.sendMessage(ChatColor.AQUA + "Your boat's Frost Durability has run out!");
+                }
+                // Bye bye, Frost Walker.
+                container.set(frostWalkerKey, PersistentDataType.INTEGER, 0);
+
+            } else if (durability == 250) {
+                for (Entity entity : boat.getPassengers()) {
+                    entity.sendMessage(ChatColor.AQUA + "Your Frost Walker boat has 250 durability remaining!");
+                }
+            }
+            container.set(durabilityKey, PersistentDataType.INTEGER, durability - 1);
         }
     }
 
     @EventHandler
     public void onBoatDestroy(VehicleDestroyEvent event) {
-        if (frostBoats.containsKey(event.getVehicle().getEntityId())) {
-            Boat boat = (Boat) event.getVehicle();
+        Entity entity = event.getVehicle();
+        if (!entity.getPersistentDataContainer().has(frostWalkerKey, PersistentDataType.INTEGER)) return;
+
+        if (entity.getPersistentDataContainer().get(frostWalkerKey, PersistentDataType.INTEGER) > 0) {
+            Boat boat = (Boat) entity;
 
             // If the player is in creative mode don't worry about this, let the boat just disappear
-            if (event.getAttacker() instanceof Player) {
-                Player p = (Player) event.getAttacker();
+            if (event.getAttacker() instanceof Player p) {
                 if (p.getGameMode() == GameMode.CREATIVE) {
-                    frostBoats.remove(boat.getEntityId());
                     return;
                 }
             }
@@ -97,58 +147,44 @@ public class Listeners implements Listener {
 
             World w = boat.getWorld();
             ItemStack item = null;
+            PersistentDataContainer container = boat.getPersistentDataContainer();
 
             // Make sure we're giving the right boat type back
             // TODO Apparently mangrove boats are of TreeSpecies.GENERIC. Weird.
             if (boat instanceof ChestBoat) {
-                switch (boat.getWoodType()) {
-                    case ACACIA:
-                        item = new ItemStack(Material.ACACIA_CHEST_BOAT);
-                        break;
-                    case BIRCH:
-                        item = new ItemStack(Material.BIRCH_CHEST_BOAT);
-                        break;
-                    case JUNGLE:
-                        item = new ItemStack(Material.JUNGLE_CHEST_BOAT);
-                        break;
-                    case GENERIC:
-                        item = new ItemStack(Material.OAK_CHEST_BOAT);
-                        break;
-                    case REDWOOD:
-                        item = new ItemStack(Material.SPRUCE_CHEST_BOAT);
-                        break;
-                    case DARK_OAK:
-                        item = new ItemStack(Material.DARK_OAK_CHEST_BOAT);
-                        break;
-                }
+                item = switch (boat.getWoodType()) {
+                    case ACACIA -> new ItemStack(Material.ACACIA_CHEST_BOAT);
+                    case BIRCH -> new ItemStack(Material.BIRCH_CHEST_BOAT);
+                    case JUNGLE -> new ItemStack(Material.JUNGLE_CHEST_BOAT);
+                    case GENERIC -> new ItemStack(Material.OAK_CHEST_BOAT);
+                    case REDWOOD -> new ItemStack(Material.SPRUCE_CHEST_BOAT);
+                    case DARK_OAK -> new ItemStack(Material.DARK_OAK_CHEST_BOAT);
+                };
             } else {
-                switch (boat.getWoodType()) {
-                    case ACACIA:
-                        item = new ItemStack(Material.ACACIA_BOAT);
-                        break;
-                    case BIRCH:
-                        item = new ItemStack(Material.BIRCH_BOAT);
-                        break;
-                    case JUNGLE:
-                        item = new ItemStack(Material.JUNGLE_BOAT);
-                        break;
-                    case GENERIC:
-                        item = new ItemStack(Material.OAK_BOAT);
-                        break;
-                    case REDWOOD:
-                        item = new ItemStack(Material.SPRUCE_BOAT);
-                        break;
-                    case DARK_OAK:
-                        item = new ItemStack(Material.DARK_OAK_BOAT);
-                        break;
-                }
+                item = switch (boat.getWoodType()) {
+                    case ACACIA -> new ItemStack(Material.ACACIA_BOAT);
+                    case BIRCH -> new ItemStack(Material.BIRCH_BOAT);
+                    case JUNGLE -> new ItemStack(Material.JUNGLE_BOAT);
+                    case GENERIC -> new ItemStack(Material.OAK_BOAT);
+                    case REDWOOD -> new ItemStack(Material.SPRUCE_BOAT);
+                    case DARK_OAK -> new ItemStack(Material.DARK_OAK_BOAT);
+                };
             }
 
             // Ensure their frost walker is retained
-            item.addUnsafeEnchantment(Enchantment.FROST_WALKER, frostBoats.get(boat.getEntityId()));
+            item.addUnsafeEnchantment(Enchantment.FROST_WALKER, container.get(frostWalkerKey, PersistentDataType.INTEGER));
 
-            // Remove from the Map
-            frostBoats.remove(boat.getEntityId());
+            // Transfer the remaining durability to the item
+            int durability = container.get(durabilityKey, PersistentDataType.INTEGER);
+            ItemMeta meta = item.getItemMeta();
+            meta.getPersistentDataContainer().set(durabilityKey, PersistentDataType.INTEGER,
+                    durability);
+
+            // Add a line in the lore that says how much durability is left
+            meta.setLore(Collections.singletonList(ChatColor.DARK_GRAY + "Durability: " + durability));
+
+            // Apply the ItemMeta
+            item.setItemMeta(meta);
 
             // Kill the existing boat
             boat.remove();
@@ -156,6 +192,24 @@ public class Listeners implements Listener {
             // Give them their item back!
             w.dropItem(boat.getLocation(), item);
 
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        for (NamespacedKey key : FrostBoats.getRecipeKeys()) {
+            e.getPlayer().discoverRecipe(key);
+        }
+    }
+
+    @EventHandler
+    public void onBoatCraft(CraftItemEvent e) {
+        if (e.getRecipe() instanceof ShapedRecipe recipe) {
+            for (NamespacedKey key : FrostBoats.getRecipeKeys()) {
+                if (recipe.getKey().equals(key)) {
+                    e.getWhoClicked().getInventory().addItem(new ItemStack(Material.BUCKET, 2));
+                }
+            }
         }
     }
 }
